cda0dc066c3bb66b25c417595f68d6709e324a32
Jason Shih <jason_shih@hotmail.com>
2024-01-17 10:38:36 -0800

    Move backend code to 'backend' folder

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { getLatestPriceForEachItem } from "./database.js";
 import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.set("view engine", "ejs");    
+app.set("views", __dirname + "/views");
+
 
 app.get("/", async (req, res) => {
   const items = await getLatestPriceForEachItem();
